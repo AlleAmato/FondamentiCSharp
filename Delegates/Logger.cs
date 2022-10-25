1bf0e6367aa2aaa17fc66af0eb9f5f3f7bca6d1c
dluppoli <davide@davideluppoli.it>
2022-10-25 08:50:50 +0200

    Delegates

---
+++ Delegates/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void LogMessage<T>(T message);
+
+    internal class Logger<T>
+    {
+        private LogMessage<T> _logger;
+
+        public Logger(LogMessage<T> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Log(T message)
+        {
+            //Console.WriteLine(message);
+            _logger(message);
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger<string> l = new Logger<string>(nonRilevante);
+            l.Log("App started");
+
+            Logger<string> l2 = new Logger<string>(Console.WriteLine);
+            l2.Log("App Started");
+
+            Logger<int> l3 = new Logger<int>(nonRilevantePerInt);
+            l3.Log(12);
+
+            Console.ReadKey();
+        }
+
+        static void nonRilevantePerInt(int i)
+        {
+            Console.WriteLine(string.Format("{0,3}", i));
+        }
+
+        static void nonRilevante(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
